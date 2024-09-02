795579842f1a9a19f162afbe6af9bba0389bbb6c
Naoto3615 <supra3615@gmail.com>
2024-09-02 17:10:58 +0900

    create rootings

--- main.py
+++ main.py
@@ -21,3 +21,24 @@ class Room(BaseModel):
     room_id: int
     room_name: str = Field(max_length=12)
     capacity: int
+
+
+app = FastAPI()
+
+
+# ? indexページ [get]
+@app.get("/")
+async def index():
+    return {"message": "success"}
+
+
+# ? ユーザー登録画面 [post]
+@app.post("/users")
+async def user(user: User):
+    return user
+
+
+# ? 予約画面 [post]
+@app.post("/room")
+async def room(room: Room):
+    return room
