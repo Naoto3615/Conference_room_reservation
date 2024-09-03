ec68d2243e29923df185d3ff7a6cc506ef223cde
Naoto3615 <supra3615@gmail.com>
2024-09-03 10:44:40 +0900

    add display of room

--- app.py
+++ app.py
@@ -3,26 +3,58 @@ import json
 import requests
 import random
 
-st.title('APIテスト画面（ユーザー）')
+page = st.sidebar.selectbox('Chose your page', ['users', 'rooms'])
 
-with st.form(key='user'):
-    # ? フォームの中身
-    user_id: int = random.randint(0, 10)
-    username: str = st.text_input('ユーザー名', max_chars=12)
+if page == 'users':
+    st.title('APIテスト画面（ユーザー）')
 
-    data = {
-        'user_id': user_id,
-        'username': username
-    }
+    with st.form(key='user'):
+        # ? フォームの中身
+        user_id: int = random.randint(0, 10)
+        username: str = st.text_input('ユーザー名', max_chars=12)
 
-    submitted = st.form_submit_button(label="リクエスト送信")
+        data = {
+            'user_id': user_id,
+            'username': username
+        }
 
-    # ? 送信ボタンを押した際の挙動
-    if submitted:
-        st.json(data)
+        submitted = st.form_submit_button(label="リクエスト送信")
 
-        url = 'http://127.0.0.1:8000/users'
-        # ? 送る内容
-        res = requests.post(url,
-                            data=json.dumps(data))
-        st.write(res.json())
+        # ? 送信ボタンを押した際の挙動
+        if submitted:
+            st.json(data)
+
+            url = 'http://127.0.0.1:8000/users'
+            # ? 送る内容
+            res = requests.post(url,
+                                data=json.dumps(data))
+            st.write(res.json())
+            st.write(data)
+
+elif page == 'rooms':
+    st.title('APIテスト画面（会議室）')
+
+    with st.form(key='rooms'):
+        # ? フォームの中身
+        room_id: int = random.randint(0, 10)
+        room_name: str = st.text_input('会議室名', max_chars=12)
+        capacity: int = st.number_input('定員', step=1, max_value=300)
+
+        data = {
+            'room_id': room_id,
+            'room_name': room_name,
+            'capacity': capacity
+        }
+
+        submitted = st.form_submit_button(label="リクエスト送信")
+
+        # ? 送信ボタンを押した際の挙動
+        if submitted:
+            st.json(data)
+
+            url = 'http://127.0.0.1:8000/room'
+            # ? 送る内容
+            res = requests.post(url,
+                                data=json.dumps(data))
+            st.write(res.json())
+            st.write(data)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import datetime
 from pydantic import BaseModel, Field
 
-
+# ? 予約モデル
 class Booking(BaseModel):
     booking_id: int
     user_id: int
@@ -11,12 +11,12 @@ class Booking(BaseModel):
     start_datetime: datetime.datetime
     end_datetime: datetime.datetime
 
-
+# ? ユーザーモデル
 class User(BaseModel):
     user_id: int
     username: str = Field(max_length=12)
 
-
+# ? 会議室モデル
 class Room(BaseModel):
     room_id: int
     room_name: str = Field(max_length=12)
@@ -35,10 +35,10 @@ async def index():
 # ? ユーザー登録画面 [post]
 @app.post("/users")
 async def user(user: User):
-    return user
+    return {"user":user}
 
 
 # ? 予約画面 [post]
 @app.post("/room")
 async def room(room: Room):
-    return room
+    return {"room": room}
