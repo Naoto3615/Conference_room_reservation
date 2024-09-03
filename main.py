5bda87ab5c25a049ee6bd9d4f23804132c6a14c7
Naoto3615 <supra3615@gmail.com>
2024-09-03 17:08:54 +0900

    Creation Preparation of CRUD

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
 from fastapi import FastAPI
-import datetime
-from pydantic import BaseModel, Field
-
-
-# ? 予約モデル
-class Booking(BaseModel):
-    booking_id: int
-    user_id: int
-    room_id: int
-    booked_num: int
-    start_datetime: datetime.datetime
-    end_datetime: datetime.datetime
-
-
-# ? ユーザーモデル
-class User(BaseModel):
-    user_id: int
-    username: str = Field(max_length=12)
-
-
-# ? 会議室モデル
-class Room(BaseModel):
-    room_id: int
-    room_name: str = Field(max_length=12)
-    capacity: int
-
+from .sql_app.schemas import User,Room,Booking
 
 app = FastAPI()
 
